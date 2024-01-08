7975518480ec013a5eeb781579eb3205975f9c93
emily <emilyreese.ga@gmail.com>
2024-01-08 10:57:18 -0500

    added footer and finished style of entire site

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import './footer.css'
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'></a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/emily-reese-55488629a/"><FaLinkedin /></a>
+        <a href="https://github.com/EmiReese"><FaGithub /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; All rights reserved. </small>
+      </div>
+    </footer>
   )
 }
 
